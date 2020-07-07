d389ab5582cc46b9997675c82b50a133e1c3d8ea
FALSEGamer4 <37382895+TheFALSEGamer4@users.noreply.github.com>
2020-07-06 22:36:41 -0500

    Update and rename src/main/java/com/falsegamer/AdvancedDIYSpy/ConfigManager.java to src/main/java/com/falsegamer/AdvancedSpy/ConfigManager.java

--- src/main/java/com/falsegamer/AdvancedSpy/ConfigManager.java
+++ src/main/java/com/falsegamer/AdvancedSpy/ConfigManager.java
@@ -1,11 +1,11 @@
-package com.falsegamer.AdvancedDIYSpy;
+package com.falsegamer.AdvancedSpy;
 
 import java.util.List;
 import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
 
 public class ConfigManager {
-    private DIYspyMain plugin;
+    private spyMain plugin;
     private FileConfiguration config;
     public List<String> blacklist;
     public List<String> socialCmds;
@@ -25,7 +25,7 @@ public class ConfigManager {
         config = plugin.getConfig();
         setupConfig();
     }
-    public ConfigManager(DIYspyMain plugin) {
+    public ConfigManager(spyMain plugin) {
         this.plugin = plugin;
         this.config = plugin.getConfig();
         this.plugin.getConfig().options().copyDefaults(true);
@@ -34,18 +34,18 @@ public class ConfigManager {
     }
 
     public void setupConfig() {
-        this.blacklist = this.config.getStringList("DIYSpy.blacklist");
-        this.socialCmds = this.config.getStringList("DIYSpy.social-commands");
-        this.replyCmds = this.config.getStringList("DIYSpy.reply-commands");
-        this.prefix = this.cc(this.config.getString("DIYSpy.prefix"));
-        this.cmdSpyToggleSelf = this.cc(this.config.getString("DIYSpy.command-spy-toggle-self"));
-        this.cmdSpyToggleOthers = this.cc(this.config.getString("DIYSpy.command-spy-toggle-others"));
-        this.socialSpyToggleSelf = this.cc(this.config.getString("DIYSpy.social-spy-toggle-self"));
-        this.socialSpyToggleOthers = this.cc(this.config.getString("DIYSpy.social-spy-toggle-others"));
-        this.errorColor = this.config.getString("DIYSpy.error-message-color");
-        this.cmdSpyFormat = this.cc(this.config.getString("DIYSpy.command-spy-format"));
-        this.socialSpyFormat = this.cc(this.config.getString("DIYSpy.social-spy-format"));
-        this.reloadMessage = this.cc(this.config.getString("DIYSpy.reload-message"));
+        this.blacklist = this.config.getStringList("AdvancedSpy.blacklist");
+        this.socialCmds = this.config.getStringList("AdvancedSpy.social-commands");
+        this.replyCmds = this.config.getStringList("AdvancedSpy.reply-commands");
+        this.prefix = this.cc(this.config.getString("AdvancedSpy.prefix"));
+        this.cmdSpyToggleSelf = this.cc(this.config.getString("AdvancedSpy.command-spy-toggle-self"));
+        this.cmdSpyToggleOthers = this.cc(this.config.getString("AdvancedSpy.command-spy-toggle-others"));
+        this.socialSpyToggleSelf = this.cc(this.config.getString("AdvancedSpy.social-spy-toggle-self"));
+        this.socialSpyToggleOthers = this.cc(this.config.getString("AdvancedSpy.social-spy-toggle-others"));
+        this.errorColor = this.config.getString("AdvancedSpy.error-message-color");
+        this.cmdSpyFormat = this.cc(this.config.getString("AdvancedSpy.command-spy-format"));
+        this.socialSpyFormat = this.cc(this.config.getString("AdvancedSpy.social-spy-format"));
+        this.reloadMessage = this.cc(this.config.getString("AdvancedSpy.reload-message"));
     }
 
     public String message(String message) {
